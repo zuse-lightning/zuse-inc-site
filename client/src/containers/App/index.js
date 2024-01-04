95328caa705d385d31ed682d7bad26dc5ef30ed7
James Geib <james@zuse.com>
2024-01-04 13:06:30 -0500

    revised root index.js

---
+++ client/src/containers/App/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Test Heading</h1>
+      </>
+    );
+  }
+}
+
+export default App;
