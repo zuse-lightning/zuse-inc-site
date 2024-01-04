786a4be9ba137ec3a1c8344c2535f5a0d6ff7e78
James Geib <james@zuse.com>
2024-01-04 12:02:44 -0500

    constructed initial server file

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+const server = require("http").createServer(app);
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+//Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("c;ient/build"));
+    app.get("*", (req, res) => {
+        return res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    });
+};
+
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
