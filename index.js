e9c7a0478ba77db51fd497c24a5b9936b060d8c6
James Geib <james@zuse.com>
2024-01-04 12:11:40 -0500

    fixed typo in server file

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("c;ient/build"));
+    app.use(express.static("client/build"));
     app.get("*", (req, res) => {
         return res.sendFile(path.join(__dirname, "./client/build/index.html"));
     });
