a3ab1a0aa7a07651ab3fe13d0e0e2a948ffb75c5
James Geib <james@zuse.com>
2024-01-04 13:13:51 -0500

    added css file to App container

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import { Header } from "semantic-ui-react";
+
+import "./style.css";
 
 class App extends Component {
   render() {
     return (
       <>
-        <h1>Test Heading</h1>
+        <Header id="test-header" as="h1">Test Heading</Header>
       </>
     );
   }
