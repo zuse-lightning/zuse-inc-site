856fb321741d8aeec00a577515c998d39bcd1fa4
James Geib <james@zuse.com>
2024-01-12 16:02:28 -0500

    wrapped company logo in container

--- client/src/containers/Navbar/index.js
+++ client/src/containers/Navbar/index.js
@@ -1,27 +1,33 @@
 import React, { Component } from "react";
-import { Menu, Dropdown } from "semantic-ui-react";
+import { Menu, Dropdown, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import ZuseLogo from "../../assets/images/zuse-logo.jpg";
+
+import "./style.css";
 
 class Navbar extends Component {
 
     state = {};
 
-    handleItemClick = (e, { name }) => this.setState({activeItem: name });
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
     render() {
 
         const { activeItem } = this.state;
-        
+
         return (
             <Menu id="main-menu" secondary>
                 <Menu.Item
+                    position="left"
                     as={Link}
                     to="/"
                     name="home"
                     active={activeItem === "home"}
                     onClick={this.handleItemClick}
                 >
-                    Home
+                    <div id="zuse-logo-container">
+                        <Image size="medium" id="home-btn-img" src={ZuseLogo} />
+                    </div>
                 </Menu.Item>
                 <Menu.Item
                     as={Link}
@@ -30,7 +36,7 @@ class Navbar extends Component {
                     active={activeItem === "about"}
                     onClick={this.handleItemClick}
                 >
-                   About 
+                    About
                 </Menu.Item>
                 <Menu.Item
                     as={Link}
