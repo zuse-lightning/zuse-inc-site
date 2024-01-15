d7bb9c3ab05836a78593e7b46d554b33573daffc
James Geib <james@zuse.com>
2024-01-15 10:10:55 -0500

    made adjustments to Home and Navbar

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -7,14 +7,11 @@ class Home extends Component {
     render() {
         return (
             <>
-                <div id="home-container">
                     <Grid.Row>
                         <Grid.Column>
                             <Header as="h1">Home</Header>
                         </Grid.Column>
                     </Grid.Row>
-                </div>
-
             </>
         );
     }
