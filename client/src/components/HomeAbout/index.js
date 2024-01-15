cc825979dd01c07d8a69dd7518aa69e80667e2f5
James Geib <james@zuse.com>
2024-01-15 13:52:27 -0500

    created folders/files and initial structure of About section for Home

---
+++ client/src/components/HomeAbout/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Header, List } from "semantic-ui-react";
+
+import "./style.css";
+
+const HomeAbout = () => {
+    return (
+        <div id="home-about-container">
+            <Header as="h1" id="home-about-header">About</Header>
+        </div>
+    );
+}
