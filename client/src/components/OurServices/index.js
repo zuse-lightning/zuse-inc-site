381c486a4ab25ec989d6861872cb561f02028320
James Geib <james@zuse.com>
2024-01-16 10:47:30 -0500

    adjusted styling and structure for OurServices component

--- client/src/components/OurServices/index.js
+++ client/src/components/OurServices/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Header, Image, Button, Icon } from "semantic-ui-react";
+import { Header, Image, Button, Icon } from "semantic-ui-react";
 import ScreenPrintingIcon from "../../assets/images/screen-printing-red.png";
 import EmbroideryIcon from "../../assets/images/embroidery-red.png";
 import PromoItemsIcon from "../../assets/images/promo-items-red.png";
@@ -11,9 +11,9 @@ import "./style.css";
 const services = [
     { image: ScreenPrintingIcon, title: "Screen Printing", description: "We sure do print screens." },
     { image: EmbroideryIcon, title: "Embroidery", description: "We embroider a lot of things." },
-    { image: PromoItemsIcon, title: "Promo Items", description: "Need promo items? We GOT promo items?" },
-    { image: FulfillmentIcon, title: "Fulfillment", description: "" },
-    { image: WebStoreIcon, title: "Web Stores", description: "" }
+    { image: PromoItemsIcon, title: "Promo Items", description: "Need promo items? We GOT promo items? Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, molestiae. Voluptas optio, harum dolorem laudantium minus deleniti placeat vel cupiditate quaerat doloremque reiciendis ullam facilis recusandae numquam temporibus voluptate sint." },
+    { image: FulfillmentIcon, title: "Fulfillment", description: "We fulfill a lot of packages 'n stuff." },
+    { image: WebStoreIcon, title: "Web Stores", description: "We build Shopify stores to sell your products." }
 ];
 
 const OurServices = () => {
@@ -22,56 +22,15 @@ const OurServices = () => {
             <Header as="h1" id="our-services-header">Our Services</Header>
             <div id="services-container">
                 {services.map((service, index) => (
-                    <div className="services-col">
-                        <Card className="services-card">
-                            <Image className="services-card-image" src={service[index].image} />
-                            <Card.Header className="services-card-header">{service[index].title}</Card.Header>
-                            <Card.Description className="services-card-description">{service[index].description}</Card.Description>
-                            <Button className="services-card-btn">MORE <Icon className="services-card-icon" name="arrow right" /></Button>
-                        </Card>
+                    <div className="services-col" key={index}>
+                        <div  className="services-card">
+                            <Image className="services-card-image" src={service.image} />
+                            <div className="services-card-header">{service.title}</div>
+                            <div className="services-card-description">{service.description}</div>
+                            <Button fluid className="services-card-btn">MORE <Icon className="services-card-icon" name="arrow right" /></Button>
+                        </div>
                     </div>
                 ))}
-
-                {/* <div className="services-col">
-                    <Card className="services-card">
-                        <Image className="services-card-image" src="#" />
-                        <Card.Header className="services-card-header"></Card.Header>
-                        <Card.Description className="services-card-description">
-
-                        </Card.Description>
-                        <Button className="services-card-btn">MORE <Icon className="services-card-icon" name="arrow right" /></Button>
-                    </Card>
-                </div>
-                <div className="services-col">
-                    <Card className="services-card">
-                        <Image className="services-card-image" src="#" />
-                        <Card.Header className="services-card-header"></Card.Header>
-                        <Card.Description className="services-card-description">
-
-                        </Card.Description>
-                        <Button className="services-card-btn">MORE <Icon className="services-card-icon" name="arrow right" /></Button>
-                    </Card>
-                </div>
-                <div className="services-col">
-                    <Card className="services-card">
-                        <Image className="services-card-image" src="#" />
-                        <Card.Header className="services-card-header"></Card.Header>
-                        <Card.Description className="services-card-description">
-
-                        </Card.Description>
-                        <Button className="services-card-btn">MORE <Icon className="services-card-icon" name="arrow right" /></Button>
-                    </Card>
-                </div>
-                <div className="services-col">
-                    <Card className="services-card">
-                        <Image className="services-card-image" src="#" />
-                        <Card.Header className="services-card-header"></Card.Header>
-                        <Card.Description className="services-card-description">
-
-                        </Card.Description>
-                        <Button className="services-card-btn">MORE <Icon className="services-card-icon" name="arrow right" /></Button>
-                    </Card>
-                </div> */}
             </div>
         </div>
     );
