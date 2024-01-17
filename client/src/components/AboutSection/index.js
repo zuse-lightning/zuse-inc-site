6dbc48fafdaf402137c7648f330071551312e563
James Geib <james@zuse.com>
2024-01-17 15:44:35 -0500

    created AboutSection component folders/files

---
+++ client/src/components/AboutSection/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header, Image, Button } from "semantic-ui-react";
+
+import "./style.css";
+
+const AboutSection = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default AboutSection;
