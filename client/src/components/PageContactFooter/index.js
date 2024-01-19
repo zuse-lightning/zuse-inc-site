55c93bab47849a12b78df027e8ae58b9dda06007
James Geib <james@zuse.com>
2024-01-19 11:21:13 -0500

    made stylistic adjustments to ScreenPrinting subpage

--- client/src/components/PageContactFooter/index.js
+++ client/src/components/PageContactFooter/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Header, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 import "./style.css";
 
@@ -7,8 +8,10 @@ const PageContactFooter = () => {
     return (
         <div id="page-contact-footer">
             <Header id="page-contact-header" as="h1">Let's Get Started</Header>
-            <p id="page-contact-text">Feel free to reach out to us, no matter how many questions you have!</p>
-            <Button id="page-contact-btn">CONTACT US</Button>
+            <Header as="h3" id="page-contact-subheader">Feel free to reach out to us, no matter how many questions you have!</Header>
+            <Link to="/contact">
+                <Button id="page-contact-btn">CONTACT US</Button>
+            </Link>
         </div>
     );
 }
--- client/src/pages/Services/ServiceScreenPrinting/index.js
+++ client/src/pages/Services/ServiceScreenPrinting/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import { Header, Image } from "semantic-ui-react";
 import ScreenPrintingTemp from "../../../assets/images/screenprinting-banner.png";
 import PageContactFooter from "../../../components/PageContactFooter";
+import BlackUtility from "../../../assets/proxy/black-utility.png";
+import BlackWomenUnion from "../../../assets/proxy/black-women-union.png";
+import GreyUtility from "../../../assets/proxy/grey-utility.png";
+import WhiteWomenUnion from "../../../assets/proxy/white-women-union.png";
 
 import "./style.css";
 
-const screenPrintingResults = [];
+const screenPrintingResults = [BlackUtility, WhiteWomenUnion, GreyUtility, BlackWomenUnion];
 
 
 const ServiceScreenPrinting = (props) => {
@@ -19,10 +23,11 @@ const ServiceScreenPrinting = (props) => {
             <Image src={ScreenPrintingTemp} />
             <br />
             <Header as="h2" className="screen-printing-subheader">How It Works</Header>
+            <br />
             <p className="screen-printing-text">
-                Here at Zuse Inc., we'll screen print your T-shirts till the cows come home. Literally. Those cows take long time to get home and that's how long
+                Here at Zuse Inc., we'll screen print your T-shirts till the cows come home. Literally. Those cows take a long time to get home and that's how long
                 we're gonna be printing your shirts for. Our team of hard-working screen printers say "No" to taking breaks because they are just THAT passionate
-                about carrying out your order and making sure it is the best possible quality that your sweet, delectible, SCRUMPTIOUS money can buy. Don't believe us?
+                about carrying out your order and making sure it is the best possible quality that your sweet, delectable, SCRUMPTIOUS money can buy. Don't believe us?
                 Are you calling us liars? Then put your money where your mouth is and contact us, we'll show you what's what in the most professional manner possible!
             </p>
             <br />
