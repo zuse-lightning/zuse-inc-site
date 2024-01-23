1097c30be21dc0ce5961eb50559d51b50de902b1
James Geib <james@zuse.com>
2024-01-23 15:28:08 -0500

    cleaned up surrounding code for Catalog page

--- client/src/pages/Catalogs/index.js
+++ client/src/pages/Catalogs/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useLocation, Routes, Route } from "react-router-dom";
-import { Grid, Header } from "semantic-ui-react";
 import TopCatalogs from "../../components/TopCatalogs";
 import CatalogSanmar from "./CatalogSanmar";
 import CatalogSSActivewear from "./CatalogSSActivewear";
@@ -11,21 +10,17 @@ import CatalogRothco from "./CatalogRothco";
 const Catalogs = () => {
 
     const { pathname } = useLocation();
-    
+
     return (
         <>
-            <Grid.Row>
-                <Grid.Column>
-                    {pathname === "/catalogs" ? <TopCatalogs pathname={pathname} /> : null}
-                    <Routes>
-                        <Route exact path="sanmar" element={<CatalogSanmar />} />
-                        <Route exact path="ssactivewear" element={<CatalogSSActivewear />} />
-                        <Route exact path="alphabroder" element={<CatalogAlphaBroder />} />
-                        <Route exact path="augusta-sportswear" element={<CatalogAugusta />} />
-                        <Route exact path="rothco" element={<CatalogRothco />} />
-                    </Routes>
-                </Grid.Column>
-            </Grid.Row>
+            {pathname === "/catalogs" ? <TopCatalogs pathname={pathname} /> : null}
+            <Routes>
+                <Route exact path="sanmar" element={<CatalogSanmar />} />
+                <Route exact path="ssactivewear" element={<CatalogSSActivewear />} />
+                <Route exact path="alphabroder" element={<CatalogAlphaBroder />} />
+                <Route exact path="augusta-sportswear" element={<CatalogAugusta />} />
+                <Route exact path="rothco" element={<CatalogRothco />} />
+            </Routes>
         </>
     );
 }
