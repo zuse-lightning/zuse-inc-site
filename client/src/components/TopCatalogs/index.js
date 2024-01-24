0a38dd49173cadd2a70b3edf77a58d1bcd12ef6f
James Geib <james@zuse.com>
2024-01-24 14:01:28 -0500

    adjust links for TopCatalogs section

--- client/src/components/TopCatalogs/index.js
+++ client/src/components/TopCatalogs/index.js
@@ -6,6 +6,11 @@ import SSActivewear from "../../assets/images/catalogs/ssactivewear.webp";
 import AlphaBroder from "../../assets/images/catalogs/alphabroder.webp";
 import AugustaSportswear from "../../assets/images/catalogs/augusta-sportswear.png";
 import Rothco from "../../assets/images/catalogs/rothco.png";
+import CharlesRiver from "../../assets/images/catalogs/charles-river.png";
+import CitadelBrands from "../../assets/images/catalogs/citadel-brands.jpg";
+import CutterBuck from "../../assets/images/catalogs/cutter-buck.png";
+import Stormtech from "../../assets/images/catalogs/stormtech.png";
+import Edwards from "../../assets/images/catalogs/edwards.png";
 
 import "./style.css";
 
@@ -14,11 +19,16 @@ const TopCatalogs = (props) => {
     const { pathname } = props;
 
     const catalogs = [
-        { id: "sanmar", image: Sanmar, link: "/catalogs/sanmar" },
-        { id: "ssactivewear", image: SSActivewear, link: "/catalogs/ssactivewear" },
-        { id: "alphabroder", image: AlphaBroder, link: "/catalogs/alphabroder" },
-        { id: "augusta-sportswear", image: AugustaSportswear, link: "/catalogs/augusta-sportswear" },
-        { id: "rothco", image: Rothco, link: "/catalogs/rothco" }
+        { id: "sanmar", image: Sanmar, link: "https://sanmar.com/" },
+        { id: "ssactivewear", image: SSActivewear, link: "https://www.ssactivewear.com/" },
+        { id: "alphabroder", image: AlphaBroder, link: "https://www.alphabroder.com/home" },
+        { id: "augusta-sportswear", image: AugustaSportswear, link: "https://www.augustasportswear.com/" },
+        { id: "rothco", image: Rothco, link: "https://www.rothco.com/" },
+        { id: "charles-river", image: CharlesRiver, link: "https://www.charlesriverapparel.com/" },
+        { id: "citadel-brands", image: CitadelBrands, link: "https://www.citadelbrands.com/home_page" },
+        { id: "cutter-buck", image: CutterBuck, link: "https://cutterbuck.com/" },
+        { id: "stormtech", image: Stormtech, link: "https://www.stormtechusa.com/" },
+        { id: "edwards", image: Edwards, link: "https://www.edwardsgarment.com/"}
     ];
 
     return (
@@ -27,7 +37,7 @@ const TopCatalogs = (props) => {
             <div id="catalogs-container">
                 {catalogs.map((catalog, index) => (
                     <div className="catalog-col" key={index}>
-                        <Image as={Link} to={catalog.link} className="catalog-img" id={catalog.id} src={catalog.image} />
+                        <Image as={Link} target=":blank" to={catalog.link} className="catalog-img" id={catalog.id} src={catalog.image} />
                     </div>
                 ))}
             </div>
