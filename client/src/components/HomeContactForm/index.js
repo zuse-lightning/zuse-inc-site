ec7d685399a8f8434743f8c7db075c4fe39448a6
James Geib <james@zuse.com>
2024-01-29 08:59:56 -0500

    provided semantic grid for HomeContactForm

--- client/src/components/HomeContactForm/index.js
+++ client/src/components/HomeContactForm/index.js
@@ -44,45 +44,61 @@ const HomeContactForm = () => {
 
     return (
         <div id="home-contact-form-container">
+            <Grid>
+                <Grid.Row centered>
+                    <Grid.Column width={8}>
+                        <Header as="h1" id="home-contact-header">Request A Call Back</Header>
+                        <Header as="h3" id="home-contact-subheader">If you have any further questions, feel free to fill out your information and let us know!</Header>
+                        <Header id="home-contact-phone-number" as="h4"><Icon id="home-contact-phone-icon" name="phone" />Call: 203-675-9550</Header>
+                        <Header id="home-contact-address" as="h4"><Icon id="home-contact-map-icon" name="map pin" />54 East Industrial Road, Branford, CT 06405</Header>
+                    </Grid.Column>
+                    <Grid.Column width={8}>
+                        <form
+                            action={FORM_ENDPOINT}
+                            onSubmit={handleSubmit}
+                            method="POST"
+                        >
+                            <Grid>
+                                <Grid.Row>
+                                    <Grid.Column width={8}>
+                                        <input id="home-name-input" type="text" placeholder="Name (required)" name="name" required />
+                                    </Grid.Column>
+                                    <Grid.Column width={8}>
+                                        <input id="home-email-input" type="email" placeholder="Email (required)" name="email" required />
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={8}>
+                                        <input id="home-phone-input" type="text" placeholder="Phone Number" name="phone-number" />
+                                    </Grid.Column>
+                                    <Grid.Column width={8}>
+                                        <input id="home-company-input" type="text" placeholder="Company" name="company" />
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row>
+                                    <Grid.Column width={16}>
+                                        <textarea id="home-message-input" placeholder="Message" name="message" />
+                                    </Grid.Column>
+                                </Grid.Row>
+                                <Grid.Row centered>
+                                    <Grid.Column width={4}>
+                                        <label id="upload-label" for="art-file">Upload Your Design:</label>
+                                    </Grid.Column>
+                                    <Grid.Column width={3}>
+                                        <input type="file" {...register("file")} id="home-art-file" name="artFile" accept="image/png, image/jpeg, image/jpg" multiple />
+                                    </Grid.Column>
+                                </Grid.Row>
+                            </Grid>
+                            <button id="home-submit-btn" type="submit">Send Message</button>
+                            <div style={{ textIndent: "-99999px", whiteSpace: "nowrap", overflow: "hidden", position: "absolute" }} aria-hidden="true">
+                                <input type="text" name="_gotcha" tabindex="-1" autocomplete="off" />
+                            </div>
+                        </form>
+                    </Grid.Column>
+                </Grid.Row>
 
-            <div className="home-contact-col">
-                <Header as="h1" id="home-contact-header">Request A Call Back</Header>
-                <Header as="h3" id="home-contact-subheader">If you have any further questions, feel free to fill out your information and let us know!</Header>
-                <Header id="home-contact-phone-number" as="h4"><Icon id="home-contact-phone-icon" name="phone" />Call: 203-675-9550</Header>
-                <Header id="home-contact-address" as="h4"><Icon id="home-contact-map-icon" name="map pin" />54 East Industrial Road, Branford, CT 06405</Header>
-            </div>
-            <form
-                action={FORM_ENDPOINT}
-                onSubmit={handleSubmit}
-                method="POST"
-                className="home-contact-col"
-            >
-                <div className="home-form-field-col">
-                    <input id="home-name-input" type="text" placeholder="Name (required)" name="name" required />
-                </div>
-                <div className="home-form-field-col">
-                    <input id="home-email-input" type="email" placeholder="Email (required)" name="email" required />
-                </div>
-                <div className="home-form-field-col">
-                    <input id="home-phone-input" type="text" placeholder="Phone Number" name="phone-number" />
-                </div>
-                <div className="home-form-field-col">
-                    <input id="home-company-input" type="text" placeholder="Company" name="company" />
-                </div>
-                <div className="home-form-field-col">
-                    <textarea id="home-message-input" placeholder="Message" name="message" />
-                </div>
-                <div className="home-form-field-col">
-                    <label for="art-file">Upload Your Design:</label>
-                </div>
-                <div className="home-form-field-col">
-                    <input type="file" {...register("file")} id="home-art-file" name="artFile" accept="image/png, image/jpeg, image/jpg" multiple />
-                </div>
-                <button id="home-submit-btn" type="submit">Send Message</button>
-                <div style={{ textIndent: "-99999px", whiteSpace: "nowrap", overflow: "hidden", position: "absolute" }} aria-hidden="true">
-                    <input type="text" name="_gotcha" tabindex="-1" autocomplete="off" />
-                </div>
-            </form>
+
+            </Grid>
         </div>
     );
 }
