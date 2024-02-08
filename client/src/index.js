b5f2c3dd92ab214c4fe1c4c34244beba30db5f3c
James Geib <james@zuse.com>
2024-02-08 13:50:26 -0500

    fixed issue with page not scrolling to top upon clicking link to different page

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./containers/App";
+import ScrollToTop from "./components/ScrollToTop";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,6 +11,7 @@ const helmetContext = {};
 root.render(
   <HelmetProvider context={helmetContext}>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </HelmetProvider>
