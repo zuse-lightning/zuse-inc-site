87ed0ee80c73e57342cb70515c2d82c4fc7dc17c
James Geib <james@zuse.com>
2024-02-12 10:30:27 -0500

    created initial SEO component structure

---
+++ client/src/components/SEO/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+export default ({title, description, keywords, type, image, url}) => {
+    return (
+        <Helmet>
+            <title>{title}</title>
+            <meta name="decription" content={description} />
+            <meta name="keywords" contetn={keywords} />
+            <meta property="og:type" content={type} />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            <meta property="og:image" content={image} />
+            <meta property="og:url" content={url} />
+        </Helmet>
+    );
+};
