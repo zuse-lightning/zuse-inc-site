f69f2009406324915369b8f37f70ac93064d0dd9
James Geib <james@zuse.com>
2024-03-01 11:54:31 -0500

    deleted address element for mobile announcement bar

--- client/src/components/AnnouncementBar/MobileAnnouncement/index.js
+++ client/src/components/AnnouncementBar/MobileAnnouncement/index.js
@@ -15,9 +15,6 @@ const MobileAnnouncment = (props) => {
             <div className="announce-col">
                 <Header id="announce-phone-number" as="h4"><Icon id="announce-phone-icon" name="phone" />Call: 203-458-3295</Header>
             </div>
-            {<div className="announce-col">
-                <Header id="announce-address" as="h4"><Icon id="announce-map-icon" name="map pin" />54 East Industrial Road, Branford, CT 06405</Header>
-            </div>}
             <div className="announce-col">
                 <div className="announce-social-icon-wrapper">
                     <Image as={Link} target=":blank" to="https://www.facebook.com/profile.php?id=100091589973552" className="announce-social-icon" id="announce-facebook-icon" src={FacebookIcon} />
