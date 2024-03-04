04b83bf3f7c29402c10b67273c6b03e615f8ef44
James Geib <james@zuse.com>
2024-03-04 15:02:11 -0500

    added text to main About section

--- client/src/components/AboutSection/index.js
+++ client/src/components/AboutSection/index.js
@@ -15,11 +15,24 @@ const AboutSection = () => {
                 <div className="about-col">
                     <Header as="h1" id="about-header">We Make Your Designs Reality</Header>
                     <p id="about-text">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis dolorum eaque, nesciunt iste autem odio animi doloremque delectus ipsum ex reiciendis libero fuga, amet repudiandae.
-                        Rerum consequuntur pariatur magnam maxime. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, optio nam neque assumenda alias voluptatibus maiores accusantium? Fugiat, cumque 
-                        saepe eius incidunt quod, suscipit distinctio a labore, accusantium officiis non! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis dolorum eaque, nesciunt iste autem 
-                        odio animi doloremque delectus ipsum ex reiciendis libero fuga, amet repudiandae. Rerum consequuntur pariatur magnam maxime. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, 
-                        optio nam neque assumenda alias voluptatibus maiores accusantium? Fugiat, cumque saepe eius incidunt quod, suscipit distinctio a labore, accusantium officiis non!
+                        With over 30 years of dedicated service in the apparel industry, we take immense pride in providing 
+                        our customers with the highest standards of quality and satisfaction.
+                    </p>
+                    <p id="about-text">
+                        While there are other companies in the market like Printful and similar services, what distinguishes us 
+                        is our unwavering commitment to our clients' success. We understand that building a brand is more than 
+                        just printing designs on garments… Zuse Inc is about creating a lasting impression and forging meaningful 
+                        connections with your audience. That's why we offer a wide variety of services tailored to meet the unique 
+                        needs of each brand we work with.
+                    </p>
+                    <p id="about-text">
+                        We pride ourselves on our flexibility and adaptability. 
+                    </p>
+                    <p id="about-text">
+                        Doesn’t matter if you're a small startup or an established enterprise, we have the capacity and expertise to 
+                        collaborate with you every step of the way. From concept development to production and distribution, we provide 
+                        end-to-end solutions that streamline the process and maximize efficiency, allowing you to focus on what you do 
+                        best. We’re here to make it happen for you. 
                     </p>
                     <Link to="/contact">
                         <Button id="contact-us-btn">CONTACT US</Button>
