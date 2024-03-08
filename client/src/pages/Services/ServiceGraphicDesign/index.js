a47658b75e55dcc0aa5465ebfd3e3e6876bed318
James Geib <james@zuse.com>
2024-03-08 13:34:21 -0500

    added text for ServiceGraphicDesign subpage

--- client/src/pages/Services/ServiceGraphicDesign/index.js
+++ client/src/pages/Services/ServiceGraphicDesign/index.js
@@ -34,16 +34,23 @@ const ServiceGraphicDesign = (props) => {
                 <Header as="h2" className="graphic-design-subheader">How It Works</Header>
                 <br />
                 <p className="graphic-design-text">
-                    From sleek hoodies to casual tees, our expertise in screen printing ensures that your brand stands
-                    out with high-quality prints that capture attention and leave a lasting impression.
+                    The designing process begins with a thorough understanding for your design goals, target audience, and brand identity. We'll work closely with you to brainstorm ideas, discuss concepts, 
+                    and explore different design options until we have a clear understanding of your vision.
                 </p>
                 <br />
                 <p className="graphic-design-text">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis dolorum eaque, nesciunt iste autem odio animi doloremque delectus ipsum ex reiciendis libero fuga, amet repudiandae.
-                    Rerum consequuntur pariatur magnam maxime. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, optio nam neque assumenda alias voluptatibus maiores accusantium? Fugiat, cumque
-                    saepe eius incidunt quod, suscipit distinctio a labore, accusantium officiis non! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis dolorum eaque, nesciunt iste autem
-                    odio animi doloremque delectus ipsum ex reiciendis libero fuga, amet repudiandae. Rerum consequuntur pariatur magnam maxime. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
-                    optio nam neque assumenda alias voluptatibus maiores accusantium? Fugiat, cumque saepe eius incidunt quod, suscipit distinctio a labore, accusantium officiis non!
+                    Once we have a solid understanding of your needs, our experienced graphic designers will get to work, creating custom designs that capture the essence of your brand and communicate your 
+                    message effectively. Whether you're starting from scratch or refining an existing design, we'll take every detail into account to ensure that the final product exceeds your expectations.
+                </p>
+                <br />
+                <p className="graphic-design-text">
+                    Communication is key throughout the design process, and we'll keep you informed every step of the way. You'll have the opportunity to provide feedback and revisions, and our team will
+                    work closely with you to incorporate your input and make sure you're completely satisfied with the final result.
+                </p>
+                <br />
+                <p className="graphic-design-text">
+                    Our goal is to create designs that not only look great but also serve their intended purpose, whether it's attracting customers, building brand recognition, or conveying information. 
+                    With our attention to detail, creativity, and dedication to customer satisfaction, you can trust that your graphic design needs are in good hands.
                 </p>
                 <br />
                 {/* <Header as="h2" className="graphic-design-subheader">What You'll Get</Header>
