c86c83994082ed927a47d964c7c4c75782f8c663
James Geib <james@zuse.com>
2024-04-16 11:42:07 -0400

    temporarily commenting out additional routes

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -70,9 +70,9 @@ const App = () => {
               <Route exact path="/catalogs" element={<Catalogs location={useCurrentUrl()} />} />
               <Route exact path="/services/*" element={<Services location={useCurrentUrl()} screenWidth={width} />} />
               <Route exact path="/contact" element={<Contact location={useCurrentUrl()} />} />
-              <Route exact path="/register" element={<Register />} />
+              {/* <Route exact path="/register" element={<Register />} />
               <Route exact path="/login" element={<Login />} />
-              <Route exact path="/reviews" element={<Reviews />} />
+              <Route exact path="/reviews" element={<Reviews />} /> */}
             </Routes>
           </div>
         </Sidebar.Pusher>
