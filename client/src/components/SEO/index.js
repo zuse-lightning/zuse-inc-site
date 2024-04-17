df797cfc331139dcc2f66497053b2b9627e7ac56
James Geib <james@zuse.com>
2024-04-17 11:11:48 -0400

    testing whichWebsite function with favicon in SEO component

--- client/src/components/SEO/index.js
+++ client/src/components/SEO/index.js
@@ -4,7 +4,13 @@ import { Helmet } from "react-helmet-async";
 export default ({location, whichWebsite, title, description, keywords, type, image, url}) => {
     return (
         <Helmet>
-            {location.includes("americancontractprinting.com") ? <link rel="icon" href="https://zuse-inc-bucket.s3.amazonaws.com/ACP-favicon.png" /> : <link rel="icon" href="https://zuse-inc-bucket.s3.amazonaws.com/zuse-favicon.png" />}
+            {whichWebsite(
+                location, 
+                <link rel="icon" href="https://zuse-inc-bucket.s3.amazonaws.com/zuse-favicon.png" />, 
+                <link rel="icon" href="https://zuse-inc-bucket.s3.amazonaws.com/ACP-favicon.png" />, 
+                <link rel="icon" href="https://zuse-inc-bucket.s3.amazonaws.com/Union-Logo.png" />
+            )}
+            {/* {location.includes("americancontractprinting.com") ? <link rel="icon" href="https://zuse-inc-bucket.s3.amazonaws.com/ACP-favicon.png" /> : <link rel="icon" href="https://zuse-inc-bucket.s3.amazonaws.com/zuse-favicon.png" />} */}
             <title>{location.includes("americancontractprinting.com") ? "American Contract Printing" : "Zuse Inc."} - {title}</title>
             <meta name="decription" content={description} />
             <meta name="keywords" content={keywords} />
