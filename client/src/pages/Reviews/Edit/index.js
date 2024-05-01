ff38c5ea7403bdf85d1e52d5c606a4503252c024
James Geib <james@zuse.com>
2024-05-01 13:36:59 -0400

    other additions

---
+++ client/src/pages/Reviews/Edit/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import moment from "moment";
+
+const Edit = (props) => {
+    return (
+        <h1>Edit</h1>
+    );
+}
+
+export default Edit;
---
+++ client/src/pages/Reviews/Review/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import moment from "moment";
+
+const Review = (props) => {
+    return (
+        <h1>Review</h1>
+    );
+}
+
+export default Review;
