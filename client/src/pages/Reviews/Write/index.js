96bbf99e06da4fbf7144df18a9440c3cf29c861e
James Geib <james@zuse.com>
2024-05-01 13:36:12 -0400

    created initial structures for Edit, Review, and Write subpages

---
+++ client/src/pages/Reviews/Write/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import moment from "moment";
+
+const Write = (props) => {
+    return (
+        <h1>Write</h1>
+    );
+}
+
+export default Write;
