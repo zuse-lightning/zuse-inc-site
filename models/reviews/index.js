c756c2f01cffb705be97650f0f50e52c78ec4ea7
James Geib <james@zuse.com>
2024-05-20 14:56:28 -0400

    adjusted SQL queries for reviews route in back-end to take into account the different tables

--- models/reviews/index.js
+++ models/reviews/index.js
@@ -1,13 +1,29 @@
-const getAllReviews = "SELECT * FROM reviews;";
-const getReviewByIds = "SELECT r.id, `first_name`, `last_name`, `text`, `rating`, `date` FROM users u JOIN reviews r ON u.id = r.uid WHERE r.id = ?;";
-const addUserReview = "INSERT INTO reviews (`uid`, `text`, `rating`, `date`) VALUES (?);";
-const deleteUserReview = "DELETE FROM reviews WHERE `id` = ? AND `uid` = ?;";
-const updateUserReview = "UPDATE reviews SET `text` = ?, `rating` = ? WHERE `id` = ? AND `uid` = ?;";
+const zuse = {
+    getAllReviews: "SELECT * FROM zuse.reviews;",
+    getReviewByIds: "SELECT r.id, `first_name`, `last_name`, `text`, `rating`, `date` FROM zuse.users u JOIN zuse.reviews r ON u.id = r.uid WHERE r.id = ?;",
+    addUserReview: "INSERT INTO zuse.reviews (`uid`, `text`, `rating`, `date`) VALUES (?);",
+    deleteUserReview: "DELETE FROM zuse.reviews WHERE `id` = ? AND `uid` = ?;",
+    updateUserReview: "UPDATE zuse.reviews SET `text` = ?, `rating` = ? WHERE `id` = ? AND `uid` = ?;"    
+};
+
+const acp = {
+    getAllReviews: "SELECT * FROM american_contract.reviews;",
+    getReviewByIds: "SELECT r.id, `first_name`, `last_name`, `text`, `rating`, `date` FROM american_contract.users u JOIN american_contract.reviews r ON u.id = r.uid WHERE r.id = ?;",
+    addUserReview: "INSERT INTO american_contract.reviews (`uid`, `text`, `rating`, `date`) VALUES (?);",
+    deleteUserReview: "DELETE FROM american_contract.reviews WHERE `id` = ? AND `uid` = ?;",
+    updateUserReview: "UPDATE american_contract.reviews SET `text` = ?, `rating` = ? WHERE `id` = ? AND `uid` = ?;"    
+};
+
+const union = {
+    getAllReviews: "SELECT * FROM union_print.reviews;",
+    getReviewByIds: "SELECT r.id, `first_name`, `last_name`, `text`, `rating`, `date` FROM union_print.users u JOIN union_print.reviews r ON u.id = r.uid WHERE r.id = ?;",
+    addUserReview: "INSERT INTO union_print.reviews (`uid`, `text`, `rating`, `date`) VALUES (?);",
+    deleteUserReview: "DELETE FROM union_print.reviews WHERE `id` = ? AND `uid` = ?;",
+    updateUserReview: "UPDATE union_print.reviews SET `text` = ?, `rating` = ? WHERE `id` = ? AND `uid` = ?;"    
+};
 
 module.exports = {
-    getAllReviews,
-    getReviewByIds,
-    addUserReview,
-    deleteUserReview,
-    updateUserReview
+    zuse,
+    acp,
+    union
 };
