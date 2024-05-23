85e4e6fb60bd48a07b78fdfa1c292aebeb6a0725
James Geib <james@zuse.com>
2024-05-23 09:00:32 -0400

    constructed alternative method for uploading images to s3 via node

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const AWS = require("aws-sdk");
-const s3 = new AWS.S3();
 const multer = require("multer");
+const multerS3 = require("multer-s3");
+
+AWS.config.update({
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    region: process.env.AWS_REGION
+});
 
 const app = express();
-
+const s3 = new AWS.S3();
 const server = require("http").createServer(app);
 const path = require("path");
 const routes = require("./routes");
@@ -16,30 +23,21 @@ const PORT = process.env.PORT || 3001;
 //Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
 app.use(cookieParser());
 
 //Uploading files
 const upload = multer({
-    storage: multer.memoryStorage(),
-    limits: {
-        fileSize: 1024 * 1024 * 5
-    }
-});
-
-app.post("/upload", upload.single("file"), (req, res) => {
-    const params = {
-        Bucket: process.env.BUCKET_NAME,
-        Key: req.file.originalname,
-        Body: req.file.buffer
-    };
-    s3.upload(params, (err, data) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).send("Error uploading file.");
-        };
-        res.send("File uploaded successfully.");
-    });
+    storage: multerS3({
+        s3: s3,
+        acl: "public-read",
+        bucket: process.env.BUCKET_NAME,
+        key: (req, file, cb) => {
+            console.log(file);
+            cb(null, Date.now().toString() + "-" + file.originalname);
+        }
+    })
 });
 
 app.use(routes);
@@ -50,5 +48,19 @@ if (process.env.NODE_ENV === "production") {
         return res.sendFile(path.join(__dirname, "./client/build/index.html"));
     });
 };
+
+app.post("/upload", upload.array("upl", 25), (req, res, next) => {
+    res.send({
+        message: "Files uploaded successfully",
+        urls: req.files.map(file => {
+            return {
+                url: file.location,
+                name: file.key,
+                type: file.mimetype,
+                size: file.size
+            }   
+        })
+    });
+});
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
