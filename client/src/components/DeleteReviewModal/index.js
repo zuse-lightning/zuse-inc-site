357489ad77f4f7b08c840b74118e74d9ccdbddcd
James Geib <james@zuse.com>
2024-07-10 11:43:15 -0400

    constructed DeleteReviewModal component

---
+++ client/src/components/DeleteReviewModal/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Button, Header, Modal } from "semantic-ui-react";
+
+import "./style.css";
+
+const DeleteReviewModal = (props) => {
+
+    const { handleDelete } = props;
+    const [open, setOpen] = useState(false);
+
+    return (
+        <Modal
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
+            open={open}
+            trigger={
+                <Button id="review-delete-btn" icon>
+                    <Icon name="x" />
+                </Button>
+            }
+        >
+            <Modal.Header>Delete Review</Modal.Header>
+            <Modal.Content>
+                <Modal.Description>
+                    <Header>Are you sure you want to delete this review?</Header>
+                </Modal.Description>
+            </Modal.Content>
+            <Modal.Actions>
+                <Button color='black' onClick={() => setOpen(false)}>
+                    Cancel
+                </Button>
+                <Button
+                    content="Log Out"
+                    labelPosition='right'
+                    icon='x'
+                    onClick={handleDelete}
+                    negative
+                />
+            </Modal.Actions>
+        </Modal>
+    );
+};
+
+export default DeleteReviewModal;
