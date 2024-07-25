c886ffd94dd6ad7607dbb0994f6268c5d1449e09
James Geib <james@zuse.com>
2024-07-25 11:26:05 -0400

    created initial structure and styling for ForgotPassword page

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -92,8 +92,8 @@ const App = () => {
               <Route exact path="/contact" element={<Contact location={useCurrentUrl()} whichWebsite={whichWebsite} />} />
               <Route exact path="/register" element={<Register auth={auth} whichWebsite={whichWebsite} />} />
               <Route exact path="/login" element={<Login auth={auth} />} />
-              <Route exact path="/reset" element={<ResetPassword auth={auth} />} />
-              <Route exact path="/forgot" element={<ForgotPassword auth={auth} />} />
+              <Route exact path="/reset" element={<ResetPassword auth={auth} whichWebsite={whichWebsite} />} />
+              <Route exact path="/forgot" element={<ForgotPassword auth={auth} whichWebsite={whichWebsite} />} />
               <Route exact path="/profile/:id" element={<Profile auth={auth} whichWebsite={whichWebsite} />} />
               <Route exact path="/reviews" element={<Reviews auth={auth} whichWebsite={whichWebsite} />} />
               <Route exact path="/review/:id" element={<Review auth={auth} whichWebsite={whichWebsite} />} />
--- client/src/pages/ForgotPassword/index.js
+++ client/src/pages/ForgotPassword/index.js
@@ -1,19 +1,51 @@
-import React, { useEffect } from "react";
-import { Header } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import { Header, Button, Message} from "semantic-ui-react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import "./style.css";
 
 const ForgotPassword = (props) => {
 
+    const { whichWebsite } = props;
     const { currentUser, notAuthorized } = props.auth;
+    const [inputs, setInputs] = useState({ email: "" });
+    const [err, setError] = useState(null);
+    const navigate = useNavigate();
+    const site = whichWebsite(window.location.href, "zuse", "acp", "union");
 
-    useEffect(() => {
+    const handleChange = (e) => {
+        setInputs(prev => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post(`${site}/auth/forgot`, inputs);
+            navigate("/");
+        } catch (err) {
+            setError(err.response.data);
+        };
+    };
+
+    useEffect(() => { 
         notAuthorized();
     }, [currentUser]);
 
     return (
         <div id="forgot-container">
             <Header as="h1" id="forgot-header">Forgot Password</Header>
+            <form onSubmit={handleSubmit} id="forgot-form">
+                <div className="forgot-form-col">
+                    <input id="forgot-email-input" required onChange={handleChange} value={inputs.email} name="email" type="email" placeholder="Email" />
+                </div>
+                <div className="forgot-form-col">
+                    <Button id="forgot-submit-btn" fluid type="submit">Send Reset Link</Button>
+                </div>
+                <div className="forgot-form-col">
+                    {err ? <Message id="err-msg" error>{err}</Message> : null}
+                </div>
+            </form>
         </div>
     );
 };
