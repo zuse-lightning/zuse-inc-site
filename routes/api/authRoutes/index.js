1355b0c632ed6412e7881738396cc59b83ecec34
James Geib <james@zuse.com>
2024-07-30 08:57:11 -0400

    added validateResetToken to reset route

--- routes/api/authRoutes/index.js
+++ routes/api/authRoutes/index.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const { login, logout, register, resetPassword, forgotPassword } = require("../../../controllers/authController");
+const { validateResetToken } = require("../../../controllers/emailController");
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/reset", resetPassword);
+router.post("/reset", validateResetToken, resetPassword);
 router.post("/forgot", forgotPassword);
 
 module.exports = router;
