4396d7342be087ab52eb42574fba1c16ad8ba976
James Geib <james@zuse.com>
2024-10-07 11:15:39 -0400

    deleted commented out imports from zuse, acp, and union route folders

--- routes/api/zuseRoutes/index.js
+++ routes/api/zuseRoutes/index.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const authRoutes = require("../authRoutes");
-// const userRoutes = require("./userRoutes");
 const reviewRoutes = require("../reviewRoutes");
 
 router.use("/auth", authRoutes);
