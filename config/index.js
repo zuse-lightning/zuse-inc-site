6ac6fac2a215af57a65d910feb42e39485e79191
James Geib <james@zuse.com>
2024-11-05 15:04:22 -0500

    commented out mongodb connection in config for now

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -8,21 +8,18 @@ const db = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
-const mongo = async () => {
-    try {
-        const connectionParams = {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true
-        };
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("connected to mongodb database");
-    } catch (error) {
-        console.log(error, "could not connect to mongodb database");
-    };
-};
+// const mongo = async () => {
+//     try {
+//         const connectionParams = {
+//             useNewUrlParser: true,
+//             useCreateIndex: true,
+//             useUnifiedTopology: true
+//         };
+//         await mongoose.connect(process.env.MONGO_URI);
+//         console.log("connected to mongodb database");
+//     } catch (error) {
+//         console.log(error, "could not connect to mongodb database");
+//     };
+// };
 
-module.exports = {
-    db, 
-    mongo
-};
+module.exports = db;
