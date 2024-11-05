ff973d80191804c4f14e9c0b286704e06900f0c7
James Geib <james@zuse.com>
2024-11-05 15:01:13 -0500

    adjusted .gitignore to exclude server side config file

---
+++ config/index.js
@@ -0,0 +1,28 @@
+const mysql = require("mysql");
+const mongoose = require("mongoose");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+const mongo = async () => {
+    try {
+        const connectionParams = {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        };
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("connected to mongodb database");
+    } catch (error) {
+        console.log(error, "could not connect to mongodb database");
+    };
+};
+
+module.exports = {
+    db, 
+    mongo
+};
