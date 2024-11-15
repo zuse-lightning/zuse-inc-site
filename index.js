5a9e930d5f3f9a802f58d6462b97217dbdc094f2
James Geib <james@zuse.com>
2024-11-15 13:43:09 -0500

    commented out aws code from main server file

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const crypto = require("crypto");
 const helmet = require("helmet");
-const { S3Client } = require("@aws-sdk/client-s3");
-const multer = require("multer");
-const multerS3 = require("multer-s3");
+// const { S3Client } = require("@aws-sdk/client-s3");
+// const multer = require("multer");
+// const multerS3 = require("multer-s3");
 
-let s3 = new S3Client({
-    region: process.env.AWS_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    },
-    sslEnabled: false,
-    s3ForcePathStyle: true,
-    signatureVersion: "v4"
-});
+// let s3 = new S3Client({
+//     region: process.env.AWS_REGION,
+//     credentials: {
+//         accessKeyId: process.env.AWS_ACCESS_KEY,
+//         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+//     },
+//     sslEnabled: false,
+//     s3ForcePathStyle: true,
+//     signatureVersion: "v4"
+// });
 
 const app = express();
 const server = require("http").createServer(app);
@@ -71,33 +71,33 @@ app.use(helmet({
 }));
 
 //Uploading files
-const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        acl: "public-read",
-        bucket: process.env.BUCKET_NAME,
-        key: (req, file, cb) => {
-            console.log(file);
-            const fullPath = `review-images/${file.originalname}`;
-            cb(null, fullPath);
-        }
-    })
-});
+// const upload = multer({
+//     storage: multerS3({
+//         s3: s3,
+//         acl: "public-read",
+//         bucket: process.env.BUCKET_NAME,
+//         key: (req, file, cb) => {
+//             console.log(file);
+//             const fullPath = `review-images/${file.originalname}`;
+//             cb(null, fullPath);
+//         }
+//     })
+// });
 
 app.use(routes);
 
-app.post("/upload", upload.single("writeFile"), (req, res) => {
-    console.log("uploading file");
-    console.log(req.file);
-    console.log("read files")
-    res.send({
-        message: "Files uploaded successfully",
-        url: req.file.location,
-        name: req.file.key,
-        type: req.file.mimetype,
-        size: req.file.size
-    });
-});
+// app.post("/upload", upload.single("writeFile"), (req, res) => {
+//     console.log("uploading file");
+//     console.log(req.file);
+//     console.log("read files")
+//     res.send({
+//         message: "Files uploaded successfully",
+//         url: req.file.location,
+//         name: req.file.key,
+//         type: req.file.mimetype,
+//         size: req.file.size
+//     });
+// });
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
