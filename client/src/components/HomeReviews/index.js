270f8267c1181ac972c9266b24bce5beb1df7209
James Geib <james@zuse.com>
2024-12-10 14:26:12 -0500

    adjusted styling for HomeReviews component

--- client/src/components/HomeReviews/index.js
+++ client/src/components/HomeReviews/index.js
@@ -29,14 +29,6 @@ const HomeReviews = (props) => {
 
     return (
         <div id="home-reviews-container">
-            <div id="home-reviews-headers-container">
-                <div className="home-reviews-headers-col">
-                    <Header as="h1" id="home-reviews-header">Reviews</Header>
-                </div>
-                <div className="home-reviews-headers-col">
-                    <Button as={Link} to="/write" size="massive" icon labelPosition="right" id="write-review-btn">Write A Review<Icon id="write-icon" name="edit" /></Button>
-                </div>
-            </div>
             <p className="home-reviews-text">{reviews.length > 0 ? "Don't take our word for it, listen to our satisfied customers!" : "Looks like we don't have any reviews yet, you could be the first!"}</p>
             {reviews.length > 0 ? null : <div id="home-reviews-no-result-container">
                 <Image id="home-reviews-no-result-image" src="https://zuse-inc-bucket.s3.amazonaws.com/no-results-blue.png" draggable="false" />
@@ -44,7 +36,7 @@ const HomeReviews = (props) => {
             <div id="home-reviews">
                 {reviews.map((review, index) => {
                     return (
-                        (review.rating < 3 || index >= 10) ? null : <div key={review.id} className="home-review-card-col">
+                        (review.rating < 3 || index >= 5) ? null : <div key={review.id} className="home-review-card-col">
                             <Link to={`/review/${review.id}`}><div className="home-review-card">
                                 <Image className="home-review-card-image" src={review.image} draggable="false" />
                                 <Rating className="home-review-card-rating" maxRating={5} rating={review.rating} icon="star" size="huge" disabled />
