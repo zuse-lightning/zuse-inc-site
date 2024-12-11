4aea33f469cf6eb91303f0ecccaac308ad281a54
James Geib <james@zuse.com>
2024-12-11 15:47:01 -0500

    added initial structure to other-reviews in Write page

--- client/src/pages/Write/index.js
+++ client/src/pages/Write/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Header, Rating } from "semantic-ui-react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Header, Rating, Image } from "semantic-ui-react";
+import { useLocation, useNavigate, Link } from "react-router-dom";
 import { axiosInstance } from "../../utils/api";
 import moment from "moment";
 
@@ -13,6 +13,7 @@ const Write = (props) => {
     const [rating, setRating] = useState(0);
     const [text, setText] = useState("");
     const [file, setFile] = useState(null);
+    const [submitted, setSubmitted] = useState(false);
 
     const site = whichWebsite(window.location.href, "zuse", "acp", "union");
     const navigate = useNavigate();
@@ -47,7 +48,7 @@ const Write = (props) => {
                 date: moment().format("YYYY-MM-DD"),
                 image: imgUrl.url
             });
-            navigate("/reviews");
+            setSubmitted(true);
         } catch (err) {
             console.log(err);
         };
@@ -63,6 +64,62 @@ const Write = (props) => {
     useEffect(() => {
         handleRate();
     }, [rating]);
+
+    if (submitted) {
+        return (
+            <div id="other-reviews-container">
+                <Header as="h1" id="other-reviews-header">Thank You For Leaving A Review!</Header>
+                <p className="other-reviews-text">If you liked our service, please consider leaving another review on one of the other websites below:</p>
+                <div id="other-reviews">
+                    <div className="other-review-col">
+                        <Image
+                            className="other-review-img"
+                            src="https://via.placeholder.com/150"
+                            as={Link}
+                            to={`https://www.google.com/search?q=Zuse+Inc.&rlz=1C5CHFA_enUS1106US1106&oq=zuse+inc.&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGEAyCAgCEEUYJxg7MggIAxAAGBYYHjIICAQQABgWGB4yBggFEEUYPDIGCAYQRRg9MgYIBxBFGD3SAQgxNzU5ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8#lrd=0x89e7d57560352e67:0xcf283c3f97c78848,3,,,,`}
+                            target="_blank"
+                        />
+                    </div>
+                    <div className="other-review-col">
+                        <Image
+                            className="other-review-img"
+                            src="https://via.placeholder.com/150"
+                            as={Link}
+                            to={`https://www.google.com/search?q=Zuse+Inc.&rlz=1C5CHFA_enUS1106US1106&oq=zuse+inc.&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGEAyCAgCEEUYJxg7MggIAxAAGBYYHjIICAQQABgWGB4yBggFEEUYPDIGCAYQRRg9MgYIBxBFGD3SAQgxNzU5ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8#lrd=0x89e7d57560352e67:0xcf283c3f97c78848,3,,,,`}
+                            target="_blank"
+                        />
+                    </div>
+                    <div className="other-review-col">
+                        <Image
+                            className="other-review-img"
+                            src="https://via.placeholder.com/150"
+                            as={Link}
+                            to={`https://www.glassdoor.com/mz-survey/employer/collectReview_input.htm?i=1051460&j=true&y=&c=PAGE_INFOSITE_TOP`}
+                            target="_blank"
+                        />
+                    </div>
+                    <div className="other-review-col">
+                        <Image
+                            className="other-review-img"
+                            src="https://via.placeholder.com/150"
+                            as={Link}
+                            to={`https://www.google.com/search?q=Zuse+Inc.&rlz=1C5CHFA_enUS1106US1106&oq=zuse+inc.&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGEAyCAgCEEUYJxg7MggIAxAAGBYYHjIICAQQABgWGB4yBggFEEUYPDIGCAYQRRg9MgYIBxBFGD3SAQgxNzU5ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8#lrd=0x89e7d57560352e67:0xcf283c3f97c78848,3,,,,`}
+                            target="_blank"
+                        />
+                    </div>
+                    <div className="other-review-col">
+                        <Image
+                            className="other-review-img"
+                            src="https://via.placeholder.com/150"
+                            as={Link}
+                            to={`https://www.google.com/search?q=Zuse+Inc.&rlz=1C5CHFA_enUS1106US1106&oq=zuse+inc.&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGEAyCAgCEEUYJxg7MggIAxAAGBYYHjIICAQQABgWGB4yBggFEEUYPDIGCAYQRRg9MgYIBxBFGD3SAQgxNzU5ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8#lrd=0x89e7d57560352e67:0xcf283c3f97c78848,3,,,,`}
+                            target="_blank"
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div id="write-container">
