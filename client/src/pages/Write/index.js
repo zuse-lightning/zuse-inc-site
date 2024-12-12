8a13aa7614033dcb714773c753c37e7a1210bc1d
James Geib <james@zuse.com>
2024-12-12 09:35:03 -0500

    added initial grid structure for outside review sites

--- client/src/pages/Write/index.js
+++ client/src/pages/Write/index.js
@@ -74,7 +74,7 @@ const Write = (props) => {
                     <div className="other-review-col">
                         <Image
                             className="other-review-img"
-                            src="https://via.placeholder.com/150"
+                            src="https://zuse-inc-bucket.s3.us-east-1.amazonaws.com/other-reviews-sites/google-logo.png"
                             as={Link}
                             to={`https://www.google.com/search?q=Zuse+Inc.&rlz=1C5CHFA_enUS1106US1106&oq=zuse+inc.&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGEAyCAgCEEUYJxg7MggIAxAAGBYYHjIICAQQABgWGB4yBggFEEUYPDIGCAYQRRg9MgYIBxBFGD3SAQgxNzU5ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8#lrd=0x89e7d57560352e67:0xcf283c3f97c78848,3,,,,`}
                             target="_blank"
@@ -83,16 +83,16 @@ const Write = (props) => {
                     <div className="other-review-col">
                         <Image
                             className="other-review-img"
-                            src="https://via.placeholder.com/150"
+                            src="https://zuse-inc-bucket.s3.us-east-1.amazonaws.com/other-reviews-sites/Indeed-logo.png"
                             as={Link}
-                            to={`https://www.google.com/search?q=Zuse+Inc.&rlz=1C5CHFA_enUS1106US1106&oq=zuse+inc.&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGEAyCAgCEEUYJxg7MggIAxAAGBYYHjIICAQQABgWGB4yBggFEEUYPDIGCAYQRRg9MgYIBxBFGD3SAQgxNzU5ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8#lrd=0x89e7d57560352e67:0xcf283c3f97c78848,3,,,,`}
+                            to={`https://www.indeed.com/survey/mc?cid=eaf15519bbee0117&pkind=comp%3Aheader%3Awrite-a-review-button__targeted`}
                             target="_blank"
                         />
                     </div>
                     <div className="other-review-col">
                         <Image
                             className="other-review-img"
-                            src="https://via.placeholder.com/150"
+                            src="https://zuse-inc-bucket.s3.us-east-1.amazonaws.com/other-reviews-sites/Glassdoor-logo.svg.png"
                             as={Link}
                             to={`https://www.glassdoor.com/mz-survey/employer/collectReview_input.htm?i=1051460&j=true&y=&c=PAGE_INFOSITE_TOP`}
                             target="_blank"
@@ -101,18 +101,18 @@ const Write = (props) => {
                     <div className="other-review-col">
                         <Image
                             className="other-review-img"
-                            src="https://via.placeholder.com/150"
+                            src="https://zuse-inc-bucket.s3.us-east-1.amazonaws.com/other-reviews-sites/yelp-logo.webp"
                             as={Link}
-                            to={`https://www.google.com/search?q=Zuse+Inc.&rlz=1C5CHFA_enUS1106US1106&oq=zuse+inc.&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGEAyCAgCEEUYJxg7MggIAxAAGBYYHjIICAQQABgWGB4yBggFEEUYPDIGCAYQRRg9MgYIBxBFGD3SAQgxNzU5ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8#lrd=0x89e7d57560352e67:0xcf283c3f97c78848,3,,,,`}
+                            to={`https://www.yelp.com/writeareview/biz/9XC1FlGhffp6lrnWQkriYg?return_url=%2Fbiz%2F9XC1FlGhffp6lrnWQkriYg&review_origin=biz-details-war-button`}
                             target="_blank"
                         />
                     </div>
                     <div className="other-review-col">
                         <Image
                             className="other-review-img"
-                            src="https://via.placeholder.com/150"
+                            src="https://zuse-inc-bucket.s3.us-east-1.amazonaws.com/other-reviews-sites/Better-Business-Bureau-Logo.png"
                             as={Link}
-                            to={`https://www.google.com/search?q=Zuse+Inc.&rlz=1C5CHFA_enUS1106US1106&oq=zuse+inc.&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGEAyCAgCEEUYJxg7MggIAxAAGBYYHjIICAQQABgWGB4yBggFEEUYPDIGCAYQRRg9MgYIBxBFGD3SAQgxNzU5ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8#lrd=0x89e7d57560352e67:0xcf283c3f97c78848,3,,,,`}
+                            to={`https://www.bbb.org/us/ct/branford/profile/garments-printing-lettering/all-weather-high-viz-0111-5002063/customer-reviews#leave-a-review`}
                             target="_blank"
                         />
                     </div>
