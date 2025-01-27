111afc73e198e948d51d40a42124bc09cb2dddda
James Geib <james@zuse.com>
2025-01-27 14:00:38 -0500

    added career express routes for zuse, acp, and union routes in api

--- routes/api/acpRoutes/index.js
+++ routes/api/acpRoutes/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const authRoutes = require("../authRoutes");
 const reviewRoutes = require("../reviewRoutes");
 const uploadRoutes = require("../uploadRoutes");
+const careerRoutes = require("../careerRoutes");
 
 router.use("/auth", authRoutes);
 router.use("/reviews", reviewRoutes);
 router.use("/upload", uploadRoutes);
+router.use("/careers", careerRoutes);
 
 module.exports = router;
--- routes/api/careerRoutes/index.js
+++ routes/api/careerRoutes/index.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
+const { addJob, getJobs, getJob, updateJob, deleteJob } = require("../../../controllers/careerController");
 
-
+router.get("/", getJobs);
+router.get("/:id", getJob);
+router.post("/", addJob);
+router.delete("/:id", deleteJob);
+router.put("/:id", updateJob);
 
 module.exports = router;
